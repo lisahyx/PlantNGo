52f7701d545c2e7d6c62477966ed2994b30c694a
lisahyx <lisahoyenxin@gmail.com>
2023-11-21 13:52:51 +0800

    Add firebase login with email and password

--- app/src/main/java/com/example/plantngo/LoginActivity.java
+++ app/src/main/java/com/example/plantngo/LoginActivity.java
@@ -1,31 +1,47 @@
 package com.example.plantngo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText emailEditText;
     private EditText passwordEditText;
-    private Button loginButton;
+    AlertDialog.Builder reset_alert;
+    LayoutInflater inflater;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Initialize UI components
         emailEditText = findViewById(R.id.email_editText);
         passwordEditText = findViewById(R.id.password_editText);
-        loginButton = findViewById(R.id.login_button);
+        Button loginButton = findViewById(R.id.login_button);
+        TextView signUpTextView = findViewById(R.id.sign_up_textView);
+        TextView forgotPasswordTextView = findViewById(R.id.forgotPassword_textView);
+        fAuth = FirebaseAuth.getInstance();
 
-        // Set an onClickListener for the login button
+        reset_alert = new AlertDialog.Builder(this);
+        inflater = this.getLayoutInflater();
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,25 +49,71 @@ public class LoginActivity extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
 
-                // Check if the email and password are valid (you should implement your validation logic here)
-                if (isValidCredentials(email, password)) {
-                    // If valid, proceed to the main activity or perform authentication
-                    // Show a toast message indicating success
-                    Toast.makeText(LoginActivity.this, "Login successful!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                } else {
-                    // If invalid, show an error message
-                    Toast.makeText(LoginActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+                if(email.isEmpty()) {
+                    emailEditText.setError("Email is missing");
+                    return;
                 }
+                if(password.isEmpty()) {
+                    passwordEditText.setError("Password is missing");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email,password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
-    }
 
-    // Validation logic
-    private boolean isValidCredentials(String email, String password) {
+        forgotPasswordTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = inflater.inflate(R.layout.reset_popup, null);
 
-        // You should also consider implementing authentication using Firebase, OAuth, or a similar service.
-        return email.equals("admin") && password.equals("12345");
+                reset_alert.setTitle("Reset Password?").setMessage("Enter your email to get password reset link")
+                    .setPositiveButton("Reset", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // validate email address
+                            EditText email = view.findViewById(R.id.reset_email);
+                            if(email.getText().toString().isEmpty()) {
+                                email.setError("Required field");
+                                return;
+                            }
+
+                            // send reset link
+                            fAuth.sendPasswordResetEmail(email.getText().toString())
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(LoginActivity.this, "Reset Email Sent", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }).setNegativeButton("Cancel", null)
+                    .setView(view).create().show();
+            }
+        });
+
+        signUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
