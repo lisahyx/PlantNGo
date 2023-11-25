1c28a7d04b47d5a4b4fb190ea94d1f52fee4d827
lisahyx <lisahoyenxin@gmail.com>
2023-11-25 23:07:09 +0800

    Rename file

--- app/src/main/java/com/example/plantngo/PlantReminderAdapter.java
+++ app/src/main/java/com/example/plantngo/PlantReminderAdapter.java
@@ -6,43 +6,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.AuthFailureError;
-import com.squareup.picasso.Picasso;
-
 import java.util.List;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+public class PlantReminderAdapter extends RecyclerView.Adapter<PlantReminderAdapter.ViewHolder> {
     LayoutInflater inflater;
     Context context;
     List<Plant> plants;
 
-    public Adapter(Context context, List<Plant> plants){
+    public PlantReminderAdapter(Context context, List<Plant> plants){
         this.inflater =  (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.context = context;
         this.plants = plants;
     }
 
-
-
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.plant_card,parent,false);
+        View view = inflater.inflate(R.layout.plant_reminders,parent,false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.bind(plants.get(position));
-        // bind the data
-//        holder.plantName.setText(plants.get(position).getPlantName());
-//        holder.plantReminders.setText(plants.get(position).getPlantReminders());
-//        Picasso.get().load(plants.get(position).getPlantImageUrl()).into(holder.plantImage);
     }
 
     @Override
