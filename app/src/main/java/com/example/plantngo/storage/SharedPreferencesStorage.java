1167bc03748464d42c031472eb398dcc9a99e366
lisahyx <lisahoyenxin@gmail.com>
2023-11-29 23:26:11 +0800

    Add userId

--- app/src/main/java/com/example/plantngo/storage/SharedPreferencesStorage.java
+++ app/src/main/java/com/example/plantngo/storage/SharedPreferencesStorage.java
@@ -17,8 +17,8 @@ public class SharedPreferencesStorage {
     RealtimeDatabaseStorage realtimeDatabase = new RealtimeDatabaseStorage();
 
     // Save plant names to SharedPreferences
-    public void savePlantNamesToSharedPreferences(Context context, List<String> plantNames) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("PlantPreferences", Context.MODE_PRIVATE);
+    public void savePlantNamesToSharedPreferences(Context context, String userId, List<String> plantNames) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("PlantPreferences_" + userId, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
         // Convert the list to a JSON array string
@@ -29,8 +29,8 @@ public class SharedPreferencesStorage {
     }
 
     // Retrieve plant names from SharedPreferences
-    public List<String> getPlantNamesFromSharedPreferences(Context context) {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("PlantPreferences", Context.MODE_PRIVATE);
+    public List<String> getPlantNamesFromSharedPreferences(Context context, String userId) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("PlantPreferences_" + userId, Context.MODE_PRIVATE);
 
         // Retrieve the JSON array string
         String plantNamesJson = sharedPreferences.getString("PlantNames", "");
@@ -41,12 +41,12 @@ public class SharedPreferencesStorage {
     }
 
     // Add a new plant name to the existing list in SharedPreferences
-    public void addPlantNameToSharedPreferences(Context context, String plantName) {
+    public void addPlantNameToSharedPreferences(Context context, String userId, String plantName) {
         // Save plant name
         SharedPreferencesStorage storage = new SharedPreferencesStorage();
 
         // Retrieve existing plant names
-        List<String> retrievedPlantNames = storage.getPlantNamesFromSharedPreferences(context);
+        List<String> retrievedPlantNames = storage.getPlantNamesFromSharedPreferences(context, userId);
 
         // Check for duplicates
         if (retrievedPlantNames != null && retrievedPlantNames.contains(plantName)) {
@@ -63,17 +63,17 @@ public class SharedPreferencesStorage {
         }
 
         // Save the updated list to SharedPreferences
-        storage.savePlantNamesToSharedPreferences(context, retrievedPlantNames);
+        storage.savePlantNamesToSharedPreferences(context, userId, retrievedPlantNames);
 
         realtimeDatabase.savePlantNamesToRealtimeDatabase(retrievedPlantNames);
 
         // Log all plant names
-        storage.displayAllPlantNames(context);
+        storage.displayAllPlantNames(context, userId);
     }
 
     // Display all plant names stored in SharedPreferences
-    public void displayAllPlantNames(Context context) {
-        List<String> plantNames = getPlantNamesFromSharedPreferences(context);
+    public void displayAllPlantNames(Context context, String userId) {
+        List<String> plantNames = getPlantNamesFromSharedPreferences(context, userId);
 
         if (plantNames != null) {
             for (String plantName : plantNames) {
@@ -83,21 +83,21 @@ public class SharedPreferencesStorage {
     }
 
     // Delete a plant name from SharedPreferences
-    public void deletePlantNameFromSharedPreferences(Context context, String plantName) {
+    public void deletePlantNameFromSharedPreferences(Context context, String userId, String plantName) {
         // Retrieve the existing list of plant names
-        List<String> plantNames = getPlantNamesFromSharedPreferences(context);
+        List<String> plantNames = getPlantNamesFromSharedPreferences(context, userId);
 
         if (plantNames != null) {
             // Remove the specified plant name
             plantNames.remove(plantName);
 
             // Save the updated list to SharedPreferences
-            savePlantNamesToSharedPreferences(context, plantNames);
+            savePlantNamesToSharedPreferences(context, userId, plantNames);
 
             realtimeDatabase.deletePlantNameFromRealtimeDatabase(plantName);
 
             // Log all plant names after deletion
-            displayAllPlantNames(context);
+            displayAllPlantNames(context, userId);
         }
     }
 }
