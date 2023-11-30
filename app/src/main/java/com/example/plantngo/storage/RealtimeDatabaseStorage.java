c29b43405a5c6d3052d9b2034ce118bdef4614dc
lisahyx <lisahoyenxin@gmail.com>
2023-11-30 17:41:04 +0800

    Add comments

--- app/src/main/java/com/example/plantngo/storage/RealtimeDatabaseStorage.java
+++ app/src/main/java/com/example/plantngo/storage/RealtimeDatabaseStorage.java
@@ -2,8 +2,7 @@ package com.example.plantngo.storage;
 
 import android.util.Log;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.example.plantngo.authentication.LoginActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -11,14 +10,24 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+/**
+ * The RealtimeDatabaseStorage class provides methods to interact with Firebase Realtime Database.
+ */
 public class RealtimeDatabaseStorage {
-    FirebaseAuth fAuth = FirebaseAuth.getInstance();
-    FirebaseUser fUser = fAuth.getCurrentUser();
-    String userId = fUser.getUid();
-    private DatabaseReference databaseReference = FirebaseDatabase.
-            getInstance("https://plantngo-app-default-rtdb.asia-southeast1.firebasedatabase.app/")
-            .getReference("users").child("userID: " + userId);;
 
+    // Firebase authentication instance
+    private String userId = LoginActivity.userID;
+
+    // Reference to the Firebase Realtime Database
+    private final DatabaseReference databaseReference = FirebaseDatabase
+            .getInstance("https://plantngo-app-default-rtdb.asia-southeast1.firebasedatabase.app/")
+            .getReference("users").child("userID: " + userId);
+
+    /**
+     * Saves a list of plant names to the Realtime Database along with the current date.
+     *
+     * @param plantNames The list of plant names to be saved.
+     */
     public void savePlantNamesToRealtimeDatabase(List<String> plantNames) {
         // Save each plant name individually
         for (String plantName : plantNames) {
@@ -30,6 +39,11 @@ public class RealtimeDatabaseStorage {
         }
     }
 
+    /**
+     * Deletes a plant name from the Realtime Database.
+     *
+     * @param plantName The name of the plant to be deleted.
+     */
     public void deletePlantNameFromRealtimeDatabase(String plantName) {
         // Remove the plant name node from the Realtime Database
         databaseReference.child("plants").child(plantName).removeValue()
@@ -39,6 +53,11 @@ public class RealtimeDatabaseStorage {
                         Log.w("RealtimeDatabase", "Error deleting plant name", e));
     }
 
+    /**
+     * Gets the current date in the "yyyy-MM-dd" format.
+     *
+     * @return The current date.
+     */
     public String getCurrentDate() {
         // Get the current date
         LocalDate currentDate = LocalDate.now();
@@ -48,6 +67,11 @@ public class RealtimeDatabaseStorage {
         return currentDate.format(formatter);
     }
 
+    /**
+     * Gets the DatabaseReference instance for further database interactions.
+     *
+     * @return The DatabaseReference instance.
+     */
     public DatabaseReference getDatabaseReference() {
         return this.databaseReference;
     }
